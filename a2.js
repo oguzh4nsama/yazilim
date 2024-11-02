5df0ccc39bf6794c97618614646a282d26948523
oguzh4nsama <154737603+oguzh4nsama@users.noreply.github.com>
2024-11-02 10:33:56 +0300

    Add files via upload

---
+++ a2.js
@@ -0,0 +1,6 @@
+//a2.js dosyası
+import { enuzunkelime } from "./s2.js";
+//enuzunkelime fonksunu s2.jsye aktarır :D
+console.log(enuzunkelime("eğlence&!! ve aksiyoon zamanıııııııııı"))
+// enuzunkelime fonks içine eğlence(..) gönderir 
+
