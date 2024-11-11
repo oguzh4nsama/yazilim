3ce55a35fe973b21c667379f740c772d0bc38a29
oguzh4nsama <154737603+oguzh4nsama@users.noreply.github.com>
2024-11-11 11:55:51 +0300

    görünüm

--- ft_indirim_hesaplama/fiyat_hesabi.js
+++ ft_indirim_hesaplama/fiyat_hesabi.js
@@ -1,11 +1,12 @@
 let fiyat = document.getElementById("fiyat");
 let indirim = document.getElementById("indirim"); //indirimi çekiyo
-function hesapla(){  // tıklanınca napıcağını söylüyo
+function hesapla() {  // tıklanınca napıcağını söylüyo
 
     let fiyat_miktari = fiyat.value; //değerini alıyo
     let indirim_miktari = indirim.value; //değerini alıyo
-    let indirimli_fiyat = fiyat_miktari - (fiyat_miktari * indirim_miktari/100)
-    alert(indirimli_fiyat);
+    let indirimli_fiyat = fiyat_miktari - (fiyat_miktari * indirim_miktari / 100)
+    let son_fiyat = document.getElementById("cikti").innerHTML;
+    document.getElementById("cikti" + son_fiyat).innerhtml 
 }
 
 
