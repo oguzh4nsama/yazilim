254591d5c55d34142e1ae0008fc5fb6856a7990c
oguzh4nsama <154737603+oguzh4nsama@users.noreply.github.com>
2024-11-11 11:33:07 +0300

    ön plan

---
+++ ft_indirim_hesaplama/fiyat_hesabi.js
@@ -0,0 +1,20 @@
+let fiyat = document.getElementById("fiyat");
+let indirim = document.getElementById("indirim"); //indirimi çekiyo
+function hesapla(){  // tıklanınca napıcağını söylüyo
+
+    let fiyat_miktari = fiyat.value; //değerini alıyo
+    let indirim_miktari = indirim.value; //değerini alıyo
+    let indirimli_fiyat = fiyat_miktari - (fiyat_miktari * indirim_miktari/100)
+    alert(indirimli_fiyat);
+}
+
+
+
+
+
+
+
+
+
+
+
