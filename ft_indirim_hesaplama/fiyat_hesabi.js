a1e0bd66ef9cda444d2b1d9d5fecc2c0edf048c6
oguzh4nsama <154737603+oguzh4nsama@users.noreply.github.com>
2024-11-12 17:36:23 +0300

    Update fiyat_hesabi.js

--- ft_indirim_hesaplama/fiyat_hesabi.js
+++ ft_indirim_hesaplama/fiyat_hesabi.js
@@ -2,15 +2,13 @@ function hesapla() {
     let fiyat1 = Number(document.getElementById("fiyat").value);
     let indirim1 = Number(document.getElementById("indirim").value); // indirimi çekiyor
     let indirim = fiyat1 - (fiyat1 * indirim1) / 100; {
-        if (indirim > indirim1) {
-            
+        if (indirim > indirim1) {            
+            document.getElementById("cikti").innerHTML = "sonuc: " + indirim;
+            /* ----------------------------------------------------------- */
             let kart = document.createElement("div");
             kart.id = "kart_divi";
             kart.className = "kart_class";
-        
-        
             document.body.appendChild(kart);
-            document.getElementById("cikti").innerHTML = "sonuc: " + indirim;
         }
         else {
             alert("Eksik ya da yanlış tuşlama yaptınız.");
