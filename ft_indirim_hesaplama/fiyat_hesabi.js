4bdbe73b9a1b4f528fa552fc39ec823882422040
oguzh4nsama <154737603+oguzh4nsama@users.noreply.github.com>
2024-11-18 16:27:16 +0300

    uyarı düzeltimi kutu ayarı

--- ft_indirim_hesaplama/fiyat_hesabi.js
+++ ft_indirim_hesaplama/fiyat_hesabi.js
@@ -1,15 +1,28 @@
 function hesapla() {
     let gelen_fiyat = Number(document.getElementById("fiyat").value);
     let ilk_indirim = Number(document.getElementById("indirimli_fiyat").value);
-    let indirimli_fiyat = gelen_fiyat - (gelen_fiyat * ilk_indirim) / 100;
-    if( gelen_fiyat == "" && ilk_indirim ==""){
-        alert("boşlukları doldurun.");
+    let indirimli_fiyat = gelen_fiyat - (gelen_fiyat * ilk_indirim) / 100;  
+    if (gelen_fiyat=="" && ilk_indirim ==""){
+        alert("lütfen ürün fiyatını ve indirimini doldurun.");
         return;
     }
-    else if (gelen_fiyat=="" || ilk_indirim ==""){
-        alert("boşluğu doldurmadınız.");
+    else if(ilk_indirim > 100){
+        alert("girilen indirim oranı 100den büyük olamaz");
         return;
     }
+    else if(ilk_indirim<0){
+        alert("girilen indirim oranı 0 dan küçük olamaz");
+        return;
+    }
+   else if( gelen_fiyat == ""){
+        alert("Ürün fiyatını doldurun.");
+        return;
+    }
+    else if(ilk_indirim == ""){
+        alert("Ürün indiriminü doldurun");
+        return;
+    }
+    
 
         if (indirimli_fiyat > ilk_indirim) {
             document.getElementById("cikti").innerHTML = "Fiyat: " + indirimli_fiyat;
@@ -65,7 +78,7 @@ function hesapla() {
             document.querySelector('.kutular').appendChild(kart);
         }
 
-        
+
         else {
             alert("Eksik ya da yanlış tuşlama yaptınız, lütfen geçerli işlem yapınız.");
         }
