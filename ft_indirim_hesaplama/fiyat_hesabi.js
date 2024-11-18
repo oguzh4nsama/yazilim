d3066cdcb6f4e216aee25922e93fbfe42d35216c
oguzh4nsama <154737603+oguzh4nsama@users.noreply.github.com>
2024-11-18 17:39:43 +0300

    Update fiyat_hesabi.js

--- ft_indirim_hesaplama/fiyat_hesabi.js
+++ ft_indirim_hesaplama/fiyat_hesabi.js
@@ -2,31 +2,47 @@ function hesapla() {
     let gelen_fiyat = Number(document.getElementById("fiyat").value);
     let indirim = Number(document.getElementById("indirimli_fiyat").value);
     let indirimli_fiyat = gelen_fiyat - (gelen_fiyat * indirim) / 100;
-    if (gelen_fiyat == "" && indirim == "") {
+    if (gelen_fiyat == "" && indirim == "") 
+    {
         document.getElementById("yanlis_cikti").innerHTML = "Lütfen ürün fiyatını ve indirimini doldurun.";
 
         return;
-    } else if (indirim > 100) {
+    } 
+
+    else if (indirim > 100) 
+    {
         document.getElementById("yanlis_cikti").innerHTML = "Girilen indirim oranı 100den büyük olamaz";
         return;
-    } else if (indirim < 0) {
+    } 
+
+    else if (indirim < 0) 
+    {
         document.getElementById("yanlis_cikti").innerHTML = "Girilen indirim oranı 0 dan küçük olamaz";
         return;
-    } else if (gelen_fiyat == "") {
+    } 
+
+    else if (gelen_fiyat == "") 
+    {
         document.getElementById("yanlis_cikti").innerHTML = "Ürün fiyatını doldurunuz.";
         return;
-    } else if (indirim == "") {
+    } 
+
+    else if (indirim == "") 
+    {
         document.getElementById("yanlis_cikti").innerHTML = "Ürün indirimini doldurunuz.";
         return;
     }
 
-    if (indirimli_fiyat > indirim) {
+    if (indirimli_fiyat > indirim)
+    {
         document.getElementById("cikti").innerHTML = "Fiyat: " + indirimli_fiyat;
         let kart = document.createElement("div");
         document.getElementById("yanlis_cikti").value = "";
         kart.id = "kart_divi";
         kart.className = "kart_class";
-        if (indirimli_fiyat > indirim) {
+
+        if (indirimli_fiyat > indirim) 
+        {
             document.getElementById("cikti").innerHTML = "Fiyat: " + indirimli_fiyat;
             let kart = document.createElement("div");
             kart.id = "kart_divi";
@@ -42,7 +58,9 @@ function hesapla() {
             document.getElementById("indirimli_fiyat").value = "";
             document.getElementById("yanlis_cikti").value = "";
         }
-    } else if (indirimli_fiyat > 0) {
+    } 
+    else if (indirimli_fiyat > 0) 
+    {
         document.getElementById("cikti").innerHTML = "Fiyat: " + indirimli_fiyat;
         let kart = document.createElement("div");
         kart.id = "kart_divi";
@@ -54,7 +72,9 @@ function hesapla() {
         let gelen_hali = document.createTextNode("İndirimsiz hali: " + gelen_fiyat.toFixed(2) + "\n",);
         kart.appendChild(gelen_hali);
         document.querySelector(".kutular").appendChild(kart);
-    } else if (indirimli_fiyat > 0 || indirimli_fiyat === 0) {
+    } 
+    else if (indirimli_fiyat > 0 || indirimli_fiyat === 0) 
+    {
         document.getElementById("cikti").innerHTML = "Fiyat: " + indirimli_fiyat;
         let kart = document.createElement("div");
         kart.id = "kart_divi";
@@ -67,7 +87,9 @@ function hesapla() {
         kart.appendChild(gelen_hali);
         document.querySelector(".kutular").appendChild(kart);
         document.getElementById("yanlis_cikti").value = "";
-    } else {
+    }
+     else 
+    {
       
         document.getElementById("yanlis_cikti").innerHTML = "Eksik tuşlama yaptınız, lütfen geçerli işlem yapınız.";
         document.getElementById("yanlis_cikti").value = "";
