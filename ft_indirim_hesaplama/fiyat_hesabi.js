18f18986a92f6d57ace483707f041f8b928102fe
oguzh4nsama <154737603+oguzh4nsama@users.noreply.github.com>
2024-11-20 17:01:20 +0300

    benimkodumunçalışanhali

--- ft_indirim_hesaplama/fiyat_hesabi.js
+++ ft_indirim_hesaplama/fiyat_hesabi.js
@@ -6,55 +6,53 @@ function hesapla() {
     let indirim_orani = Number(indirim.value);
 
     //işlem
-    let indirimli_fiyat = gelen_fiyat - (gelen_fiyat * indirim_orani) / 100; 
-    
+    let indirimli_fiyat = gelen_fiyat - (gelen_fiyat * indirim_orani) / 100;
+
     //temizleme
     let hata = document.getElementById("yanlis_cikti");
     hata.innerHTML = "";
-        
-         //hata belirtisi
-        fiyat.style.border = "3px solid darkred";  
-        indirim.style.border = "3px solid darkred"; 
 
-        //sorgu
-        if (gelen_fiyat == "" && indirim_orani == "") { 
+    //hata belirtisi
+    fiyat.style.border = "3px solid darkred";
+    indirim.style.border = "3px solid darkred";
+    function kirmizi_yeşil() {
+        if (gelen_fiyat == "" && indirim_orani == "") {
 
             hata.innerHTML = "Lütfen ürün fiyatını ve indirimini doldurun.";
             return;
         }
-        if (gelen_fiyat <= 0) {
+        else if (gelen_fiyat <= 0) {
             hata.innerHTML = "Lütfen ürün fiyatını pozitif bir değer giriniz.";
             return;
         }
-        if (indirim_orani <= 0) {
+        else if (indirim_orani <= 0) {
             hata.innerHTML = "Lütfen indirim oranını pozitif bir değer giriniz.";
             return;
         }
-        if (indirim_orani > 100) {
+        else if (indirim_orani > 100) {
             hata.innerHTML = "Girilen indirim oranı 100'den büyük olamaz.";
             return;
         }
+    }
     //doğru belirtisi  
     fiyat.style.border = "3px solid green";
     indirim.style.border = "3px solid green";
-    fiyat.value = "";   
+
+    fiyat.value = "";
     indirim.value = "";
 
     //indirimli halini düz gösterme
-    document.getElementById("cikti").innerHTML = "Fiyat: " + indirimli_fiyat.toFixed(2) + " TL";  
+    document.getElementById("cikti").innerHTML = "Fiyat: " + indirimli_fiyat.toFixed(2) + " TL";
 
     //div br strike yaratma
-    let kart = document.createElement("div");   
+    let kart = document.createElement("div");
     kart.id = "kart_divi";
     kart.className = "kart_class";
-    let bosluk = document.createElement("br"); 
+    let bosluk = document.createElement("br");
     let cizgi = document.createElement("strike");
-        
-     //sen gidicen
-    cizgi.style.color = "red"; 
 
     //indirimsiz hali gelen_Fiyat-fiyat
-    cizgi.textContent = "İndirimsiz hali: " + gelen_fiyat.toFixed(2) + " TL"; 
+    cizgi.textContent = "İndirimsiz hali: " + gelen_fiyat.toFixed(2) + " TL";
     kart.appendChild(cizgi);
 
     //indirimli hali indirim-indirimli_orani
@@ -69,7 +67,4 @@ function hesapla() {
     document.querySelector(".kutular").appendChild(kart);
 }
 
-/* 
-kodu anlamıyom... :b
-
-*/
+/* ez*/
